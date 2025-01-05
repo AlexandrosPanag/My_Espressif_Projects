44da12a97cb2bce64a0b9c74c479b2e8c2c394bb
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2025-01-05 20:01:13 +0200

    Add files via upload

---
+++ ESP8266/DHT11/ESP8266_DHT11.ino
@@ -0,0 +1,62 @@
+// By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+// January 05/01/2025
+
+
+// connect the sensor : 
+// DHT11             ESP8266
+// left sensor pin   D4
+// VDD/VIN           DIGITAL 3V3
+// GND               DIGITAL GND
+
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+// Please support the original library authors
+#include "DHT.h"
+
+#define DHTPIN 2     // connect to the D4
+
+#define DHTTYPE DHT11   // DHT 11
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHT11 test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a second between measurements.
+  delay(1000);
+
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
